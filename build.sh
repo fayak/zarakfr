c5babb0f9366542f54c015998496727852275c05
zarak <cyril@fayak.com>
2021-09-19 23:46:13 +0000

    Fix build script
    Signed-off-by: zarak <cyril@fayak.com>

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ docker run --rm \
 	-v $(pwd)/site:/srv/jekyll \
 	-v $(pwd)/bundle:/usr/local/bundle \
 	-e JEKYLL_ENV=production \
-	jekyll/jekyll jekyll build
+	jekyll/jekyll /bin/bash -c "chmod a+w /srv/jekyll/Gemfile.lock && chmod 777 /srv/jekyll && jekyll build"
 
 cp -r site/_site/* /srv/www/zarak.fr
